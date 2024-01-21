f882ed9bbe9b9ef01cd9c9a84a851833a570c990
Suraj Kumar <56630693+surajkumar4aug@users.noreply.github.com>
2024-01-21 17:28:15 +0530

    Delete Stockinventory directory

--- Stockinventory/user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Address, User, CustomerDetails
-# Register your models here.
-admin.site.register(Address)
-admin.site.register(User)
-admin.site.register(CustomerDetails),
